051c759aac7aa0f1e9facf55d14c534cc091fba2
Kirill Lappo <kirill-lappo@outlook.com>
2025-03-17 13:36:33 +0300

    zed settings

--- lua/nvim-daily-quotes.lua
+++ lua/nvim-daily-quotes.lua
@@ -6,19 +6,19 @@ local quotes = require("quote-lines")
 ---@param day integer
 ---@return string
 local function get_quote(day)
-	local group_size = 32
-	local groups = #quotes / group_size
-	local group_index = math.random(groups)
-	local index = group_index * group_size + group_size % day
+  local group_size = 32
+  local groups = #quotes / group_size
+  local group_index = math.random(groups)
+  local index = group_index * group_size + group_size % day
 
-	return quotes[index]
+  return quotes[index]
 end
 
 --- Returns quote of a day
 ---@return string
 function M.get_qod()
-	local day = os.date("*t").day
-	return get_quote(day)
+  local day = os.date("*t").day
+  return get_quote(day)
 end
 
 return M
