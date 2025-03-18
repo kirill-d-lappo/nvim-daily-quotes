b030f90a5922046f771c8f9a30244c42e9d4b556
Kirill Lappo <kirill-lappo@outlook.com>
2025-03-18 21:08:43 +0300

    simplified get quote logic

--- lua/nvim-daily-quotes.lua
+++ lua/nvim-daily-quotes.lua
@@ -2,28 +2,16 @@ local M = {}
 
 local quotes = require("quote-lines")
 
---- Returns quote index according to selected day
+--- Returns quote according to selected day
 ---@param day_of_year integer
 ---@return string
 local function get_quote(day_of_year)
-  local month_group_size = 31
   local quotes_len = #quotes
 
-  local month_numbers = math.floor(quotes_len / month_group_size)
-  local month_number = math.random(month_numbers)
-  local month_index = month_number - 1;
-
-  local month_index_group_size = month_group_size
-  if month_number == month_numbers then
-    month_group_size = quotes_len % month_group_size
+  local index = day_of_year
+  if (index >= quotes_len) then
+    index = day_of_year % quotes_len
   end
-  if month_index_group_size <= 0 then
-    month_group_size = month_group_size
-  end
-
-  local day_offset = day_of_year % month_group_size;
-
-  local index = month_index * month_group_size + day_offset
 
   return quotes[index]
 end
